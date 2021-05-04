6f847117b29c5c8554658a1a5da9e34c00805f2d
BIT Student <marinastanicic94@gmail.com>
2021-05-04 11:14:09 +0200

    switch commit

---
+++ switch/switch.js
@@ -0,0 +1,240 @@
+//Prvi zadatak
+
+var day = "1";
+
+switch (day) {
+    case "1":
+        console.log("Ponedeljak!");
+        break;
+    case "2":
+        console.log("Utorak!");
+        break;
+    case "3":
+        console.log("Sreda!");
+        break;
+    case "4":
+        console.log("Cetvrtak!");
+        break;
+    case "5":
+        console.log("Petak!");
+        break;
+    default:
+        console.log("Not a day");
+}
+
+//Drugi zadatak
+
+var day = "10";
+
+switch (day) {
+    case "1":
+        console.log("Ponedeljak!");
+        break;
+    case "2":
+        console.log("Utorak!");
+        break;
+    case "3":
+        console.log("Sreda!");
+        break;
+    case "4":
+        console.log("Cetvrtak!");
+        break;
+    case "5":
+        console.log("Petak!");
+        break;
+    default:
+        console.log("Input must be a number between 1 and 7");
+}
+
+//Treci zadatak
+
+var day = 2;
+
+switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log("It s weekday!");
+        break;
+    case 6:
+    case 7:
+        console.log("It s weekday");
+        break;
+    default:
+        console.log("Input must be a number between 1 and 7!");
+}
+
+//Cetvrti zadatak
+
+var month = 13;
+
+switch (month) {
+    case 1:
+        console.log("January!");
+        break;
+    case 2:
+        console.log("February!");
+        break;
+    case 3:
+        console.log("March!");
+        break;
+    case 4:
+        console.log("April!");
+        break;
+    case 5:
+        console.log("May!");
+        break;
+    case 6:
+        console.log("June!");
+        break;
+    case 7:
+        console.log("July!");
+        break;
+    case 8:
+        console.log("Avgust!");
+        break;
+    case 9:
+        console.log("Septembar!");
+        break;
+    case 10:
+        console.log("October!");
+        break;
+    case 11:
+        console.log("November!");
+        break;
+    case 12:
+        console.log("December!");
+        break;
+    default:
+        console.log("Input must be a number between 1 and 12.");
+}
+
+
+//Peti zadatak
+
+var season = 17;
+
+switch (season) {
+    case 1:
+        console.log("Winter!");
+        break;
+    case 2:
+        console.log("Winter!");
+        break;
+    case 3:
+        console.log("Spring!");
+        break;
+    case 4:
+        console.log("Spring!");
+        break;
+    case 5:
+        console.log("Spring!");
+        break;
+    case 6:
+        console.log("Summer!");
+        break;
+    case 7:
+        console.log("Summer!");
+        break;
+    case 8:
+        console.log("Summer!");
+        break;
+    case 9:
+        console.log("Autumn!");
+        break;
+    case 10:
+        console.log("Autumn!");
+        break;
+    case 11:
+        console.log("Autumn!");
+        break;
+    case 12:
+        console.log("Winter!");
+        break;
+    default:
+        console.log("All input must be a number between 1 and 12");
+}
+
+//Sesti zadatak
+
+var grade = "T"
+
+switch (grade) {
+    case "A":
+        console.log("Good job!");
+        break;
+    case "B":
+        console.log("Pretty good!");
+        break;
+    case "C":
+        console.log("Passed!");
+        break;
+    case "D":
+        console.log("Not so good!!");
+        break;
+    case "F":
+        console.log("Failed!");
+        break;
+    default:
+        console.log("Unknown grade!");
+}
+
+//Sedmi zadatak
+
+var city1 = "Arizona";
+
+
+switch (city1) {
+    case "Belgrade":
+    case "Nis":
+    case "Cacak":
+    case "Novi Sad":
+    case "Kraljevo":
+        console.log("Serbia!");
+        break;
+    case "Barselona":
+    case "Valensija":
+    case "Madrid":
+    case "Granada":
+    case "Cordoba":
+        console.log("Spain!");
+        break;
+    case "Arizona":
+    case "New York":
+    case "Cape Code":
+    case "Los Angeles":
+    case "Chicago":
+        console.log("SAD!");
+        break;
+    case "Rome":
+    case "Palermo":
+    case "Torino":
+    case "Milano":
+    case "Venice":
+        console.log("Italy!")
+        break;
+    case "Vancuver":
+    case "Toronto":
+    case "Ontario":
+    case "Ottawa":
+    case "Winnipeg":
+        console.log("Canada!");
+        break;
+    default:
+        console.log("Please choose a different city");
+}
+
+//Osmi zadatak
+
+var a = -5;
+var b = 4;
+
+
+
+
+
+
+
+
