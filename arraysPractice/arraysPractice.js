a4208289b5083c910147090fc36ed4f707e0a359
BIT Student <marinastanicic94@gmail.com>
2021-05-04 12:14:58 +0200

    arrays

---
+++ arraysPractice/arraysPractice.js
@@ -0,0 +1,33 @@
+// Prvi zadatak
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "Avgust", "September", "October", "November", "December"]
+
+console.log(months[5], months[9], months[0]);
+
+//Drugi zadatak
+
+var daysInWeek = ["Ponedeljak", "Utorak", "Sreda", "Cetvrtak", "Petak", "Subota", "Nedelja"]
+
+console.log(daysInWeek[6]);
+
+//Treci zadatak
+
+var elements = [2, -4, 5, -2, -11];
+
+console.log(elements[1], elements[3], elements[4]);
+
+//Cetvrti zadatak
+
+var array = [5, 15, -5, 20, 12, 18, 72, 14, 9]
+
+console.log(array[1], array[3], array[5], array[6], array[8]);
+
+//Peti zadatak
+
+var longArray = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+
+console.log(longArray[0][3]);
+console.log(longArray[2]);
+console.log(longArray[2][1]);
+
+
